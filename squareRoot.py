b936f8c428f343ca263694272c02bfeef2b1995f
Balqis Kinanti Haldi Heryputri <s10204973@connect.np.edu.sg>
2022-10-29 20:33:35 +0800

    Add menu for square root

---
+++ squareRoot.py
@@ -0,0 +1,15 @@
+import math
+
+
+def squareRoot():
+    while True:
+        try:
+            number = input("\nInput the number you want to square root: ")
+            # answer = math.sqrt(float(number))
+        except ValueError:
+            print("Uh Oh, Please enter a valid number \n")
+            continue
+        else:
+            print("ANSWER")
+            # print("The square root of " + str(number) + " is: " + str(answer) + "\n")
+            break
